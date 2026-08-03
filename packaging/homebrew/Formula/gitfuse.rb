91415751f8683d95429c09fc626357bfca81b9ea
VectorSolus <helloiacon@gmail.com>
2026-08-03 09:41:49 +0530

    chore(distribution): pin checksums and add AGPL license

--- packaging/homebrew/Formula/gitfuse.rb
+++ packaging/homebrew/Formula/gitfuse.rb
@@ -8,20 +8,20 @@ class Gitfuse < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/VectorSolus/GitFuse/releases/download/v#{version}/gitfuse_v#{version}_darwin_arm64.tar.gz"
-      sha256 "TODO_REPLACE_WITH_DARWIN_ARM64_SHA256"
+      sha256 "c3bdc15bc0c7c4341e25a87ba85ad877c3aee475336b6c070cc9e3bc60a9561e"
     else
       url "https://github.com/VectorSolus/GitFuse/releases/download/v#{version}/gitfuse_v#{version}_darwin_amd64.tar.gz"
-      sha256 "TODO_REPLACE_WITH_DARWIN_AMD64_SHA256"
+      sha256 "23f9417b73551020ca3a52c456f6ddb7c361bd68ec17e9e998a01268b1aaa82c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/VectorSolus/GitFuse/releases/download/v#{version}/gitfuse_v#{version}_linux_arm64.tar.gz"
-      sha256 "TODO_REPLACE_WITH_LINUX_ARM64_SHA256"
+      sha256 "3f2b24efe00bdac4cff40808e7236f8ab07b37b36ef905dcad0288c847a4dcfd"
     else
       url "https://github.com/VectorSolus/GitFuse/releases/download/v#{version}/gitfuse_v#{version}_linux_amd64.tar.gz"
-      sha256 "TODO_REPLACE_WITH_LINUX_AMD64_SHA256"
+      sha256 "3221d497096e6f6064ba39289f601f07ea6257645307e1ddc3e0e285490e22ef"
     end
   end
 
